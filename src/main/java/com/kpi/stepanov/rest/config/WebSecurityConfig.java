2286ce5ef68884ec2890acedd4d4db4a15512add
Alexander <vaiol.ans@gmail.com>
2015-05-06 01:57:34 +0300

    final security setup
    +war file

--- src/main/java/com/kpi/stepanov/rest/config/WebSecurityConfig.java
+++ src/main/java/com/kpi/stepanov/rest/config/WebSecurityConfig.java
@@ -28,27 +28,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        http.csrf()
-//                .disable()
-//                .authorizeRequests()
-//                .antMatchers("/resources/**", "/**").permitAll()
-//                .anyRequest().permitAll()
-//                .and();
-//        http.formLogin()
-//                .loginPage("/login")
-//                .loginProcessingUrl("/j_spring_security_check")
-//                .failureUrl("/login?error")
-//                .usernameParameter("j_username")
-//                .passwordParameter("j_password")
-//                .permitAll();
-//        http.logout()
-//                .permitAll()
-//                .logoutUrl("/logout")
-//                .logoutSuccessUrl("/login?logout")
-//                .invalidateHttpSession(true);
-
-//        http.csrf()
-//            .disable();
         http.authorizeRequests()
             .antMatchers("/user").authenticated()
             .anyRequest().permitAll();
